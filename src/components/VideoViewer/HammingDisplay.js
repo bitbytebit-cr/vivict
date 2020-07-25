b6da14309e0154ac0f9f87fde1b6783fc6d78fbd
Christopher Kennedy <ckennedy@ellation.com>
2020-07-25 10:30:42 -0700

    fix hamming

--- src/components/VideoViewer/HammingDisplay.js
+++ src/components/VideoViewer/HammingDisplay.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const HammingDisplay = ({this.hamming}) => (<div className="hamming-display">{this.hamming}</div>);
+const HammingDisplay = ({hamming}) => (<div className="hamming-display">{hamming}</div>);
 
 export default HammingDisplay;
